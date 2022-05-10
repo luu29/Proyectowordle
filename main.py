4a04d486173ed5a5d3cde352ad5ea35ede521931
luu29 <103211867+luu29@users.noreply.github.com>
2022-05-10 08:10:59 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+if__name__ == "__main__":
+  archivo = open("listado.txt")
+  palabras = []
+  print(">>> FILTRANDO PALABRAS DE 5 LETRAS")
+  print("### se realiza el proceso del filtrado de palabras ###")
+  for i in range(1290)
+      lineas = archivo.readline()
+      if len(lineas) == 6:
+          palabras.append(lineas)
+  print(palabras)
+  print(">>> OPERACIÓN COMPLETADA CON EXITO")
+  
+  primero = ">>> SE ENCONTRARON "
+  segundo = len(palabras)
+  tercero = " PALABRAS DE 5 LETRAS"
+  frase = (str(primero) + str(segundo) + str(tercero))
+  print(frase)
