55b0e68bb6ccea12afadd885bca4a38776ae805a
luu29 <103211867+luu29@users.noreply.github.com>
2022-05-12 15:23:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-if__name__ == "__main__":
+#ETAPA 1
+if __name__ == "__main__":
   archivo = open("listado.txt")
   palabras = []
   print(">>> FILTRANDO PALABRAS DE 5 LETRAS")
   print("### se realiza el proceso del filtrado de palabras ###")
-  for i in range(1290)
-      lineas = archivo.readline()
-      if len(lineas) == 6:
-          palabras.append(lineas)
+  for i in range(1290):
+    lineas = archivo.readline()
+    if len(lineas) == 6:
+          palabras.append(lineas.rstrip())
   print(palabras)
   print(">>> OPERACIÓN COMPLETADA CON EXITO")
   
@@ -15,3 +16,16 @@ if__name__ == "__main__":
   tercero = " PALABRAS DE 5 LETRAS"
   frase = (str(primero) + str(segundo) + str(tercero))
   print(frase)
+#ETAPA 2
+  import random 
+
+  def selectRandom(palabras):
+    return random.choice(palabras)
+  print("PALABRA ELEGIDA AL AZAR: ", selectRandom(palabras))
+
+  while palabra == input (">>> INTRODUCIR PALABRAS DE 5 LETRAS:"):
+    if palabra in listado.txt:
+      print("gracias")
+    if len(palabra) != 5:
+      print("!!! ERROR: La palabra introducida tiene una cantidad incorrecta de caracteres. Por favor vuelva a  ingresar una palabra.")
+    
