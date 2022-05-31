fc734b1fe2ce7370e464c98dbada2ea0e518e95d
luu29 <103211867+luu29@users.noreply.github.com>
2022-05-31 09:05:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random 
+import random
+from traceback import format_exc 
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
 
 #ETAPA 1
 if __name__ == "__main__":
@@ -36,12 +40,23 @@ if __name__ == "__main__":
       
  
 #ETAPA 3
-from colorama import init, Fore
 
 #para iniciar colorama
-init()
-
-for i in range(len(random)):
-    for j in range(len(palabra)):
-        if random[i] == palabra[j]:
-            print(Fore.GREEN + palabra[j])
+random1 = random.choice(palabras)
+for a in range(1, 7):
+    palabra = input (">>> INTRODUCIR PALABRAS DE 5 LETRAS:")
+    while palabra not in palabras:
+        if palabra.isalpha:
+            print("!!! ERROR: La palabra introducida contiene caracteres que no son letras. Por favor vuelva a ingresar una palabra.")
+        if len(palabra) != 5:
+            print("!!! ERROR: La palabra introducida tiene una cantidad incorrecta de caracteres. Por favor vuelva a ingresar una palabra.")
+        print("!!! ERROR: La palabra introducida no pertenece a la lista. Por favor vuelva a ingresar una palabra.")
+        palabra = input (">>> INTRODUCIR PALABRAS DE 5 LETRAS:")
+    for i in range(len(palabra)):
+        if palabra[i] == random1[i]:
+            print(Fore.GREEN + palabra[i])
+        elif palabra[i] in random1:
+            print(Fore.YELLOW + palabra[i])
+        else:             
+            print(Fore.RED + palabra[i])
+        print("¡¡GANASTE!!")
